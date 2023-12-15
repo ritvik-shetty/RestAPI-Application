b3d9bce8b239d2f293175a074c0f7b771678557d
ritvik-s-shetty <ritvik.shetty@thoughtfocus.com>
2023-12-15 15:00:06 +0530

    Implemented Logger to application

--- api_project/application.py
+++ api_project/application.py
@@ -2,24 +2,33 @@ from flask import Flask, request, jsonify
 import sqlite3
 import logging
 
+logging.basicConfig(level=logging.DEBUG, filename="logs.log",filemode='w' , format='%(asctime)s,%(name)s:%(levelname)s:%(message)s')
+# logger=logging.getLogger(__name__)
+
+
 app = Flask(__name__)
+
 
 
 @app.route('/employee', methods=['GET','POST'])
 def posts():
-
     if request.method == 'GET':
-      
-        conn = sqlite3.connect('empdatabase.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM emp')
-        data = cursor.fetchall()
-        conn.close()
- 
-        result = [{'id': row[0], 'name': row[1], 'address':row[2], 'city':row[3], 'salary':row[4] } for row in data]
-        return jsonify(result)
+        app.logger.info("User requested a GET request")
+        try:
+            conn = sqlite3.connect('empdatabase.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM emp')
+            data = cursor.fetchall()
+            conn.close()
+            app.logger.info("Data fetched sucessfully from the database")
+            result = [{'id': row[0], 'name': row[1], 'address':row[2], 'city':row[3], 'salary':row[4] } for row in data]
+            return jsonify(result)
+        except Exception as obj:
+            logging.error("Exception Information",exc_info=True)
+
 
     elif request.method == 'POST':
+        app.logger.info("User requested a POST request")
         data = request.get_json()
 
         name = data.get('name')
@@ -30,19 +39,21 @@ def posts():
         if not name or not address or not city or not salary:
             return jsonify({'error': 'name address city and salary are required'}), 400
 
-        connection = sqlite3.connect('empdatabase.db')
-        cursor = connection.cursor()
-
-        cursor.execute('INSERT INTO emp (name, addr, city, salary) VALUES (?, ?, ?, ?)', (name, address, city, salary))
-
-        connection.commit()
-        connection.close()
-
-        return jsonify({'message': 'Employee created successfully, '+ name}), 201
-
+        try:
+            connection = sqlite3.connect('empdatabase.db')
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO emp (name, addr, city, salary) VALUES (?, ?, ?, ?)', (name, address, city, salary))
+            connection.commit()
+            connection.close()
+            app.logger.info("Data put sucessfully into the database")
+            return jsonify({'message': 'Employee created successfully, '+ name}), 201
+        
+        except Exception as obj:
+            logging.error("Exception Information",exc_info=True)
 
 @app.route('/employee/<int:emp_id>',methods=['PUT'])
 def postdata(emp_id):
+    app.logger.info(f"User requested a PUT request for employee-id:{emp_id} ")
     data = request.get_json()
 
     name = data.get('name')
@@ -53,61 +64,78 @@ def postdata(emp_id):
     if not name or not address or not city or not salary:
         return jsonify({'error': 'name address city and salary are required'}), 400
 
-    connection = sqlite3.connect('empdatabase.db')
-    cursor = connection.cursor()
+    try:
+        connection = sqlite3.connect('empdatabase.db')
+        cursor = connection.cursor()
 
-    cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
-    existing_emp = cursor.fetchone()
+        cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
+        existing_emp = cursor.fetchone()
 
-    if existing_emp is None:
-        return jsonify({'error': 'Employee not found'}), 404
+        if existing_emp is None:
+            app.logger.info("Data is not present in the database")
+            return jsonify({'error': 'Employee not found'}), 404
 
    
-    new_name = data.get('name', existing_emp[1])
-    new_address = data.get('address', existing_emp[2])
-    new_city=data.get('city',existing_emp[3])
-    new_salary=data.get('salary',existing_emp[3])
-    
-    cursor.execute('UPDATE emp SET name = ?, addr = ?, city=?, salary=? WHERE id = ?', (new_name, new_address, new_city,new_salary, emp_id))
-    connection.commit()
-    connection.close()
-    return jsonify({'message': 'Employee updated successfully '+ name}), 200
+        new_name = data.get('name', existing_emp[1])
+        new_address = data.get('address', existing_emp[2])
+        new_city=data.get('city',existing_emp[3])
+        new_salary=data.get('salary',existing_emp[3])
 
+        cursor.execute('UPDATE emp SET name = ?, addr = ?, city=?, salary=? WHERE id = ?', (new_name, new_address, new_city,new_salary, emp_id))
+        connection.commit()
+        connection.close()
+        app.logger.info("Data updated sucessfully into the database")
+        return jsonify({'message': 'Employee updated successfully '+ name}), 200
+
+    except Exception as obj:
+            logging.error("Exception Information",exc_info=True)
 
 
 @app.route('/del_employee/<int:emp_id>', methods=['DELETE'])
 def delete_emp_route(emp_id):
-    connection = sqlite3.connect('empdatabase.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
-    data = cursor.fetchone()
+    app.logger.info("User requested a DELETE request")
+    try:
+        connection = sqlite3.connect('empdatabase.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
+        data = cursor.fetchone()
 
-    if data:
-        cursor.execute('DELETE FROM emp WHERE id = ?', (emp_id,))
-        connection.commit()
-        connection.close()
-        return jsonify({'message': 'Employee deleted successfully'})
-    else:
-        connection.close()
-        return jsonify({'message': 'Improper ID'}), 404
+        if data:
+            cursor.execute('DELETE FROM emp WHERE id = ?', (emp_id,))
+            connection.commit()
+            connection.close()
+            app.logger.info("Data deleted sucessfully from the database")
+            return jsonify({'message': 'Employee deleted successfully'})
+        else:
+            connection.close()
+            app.logger.info("Data is not present in the database")
+            return jsonify({'message': 'Improper ID'}), 404
     
-
+    except Exception as obj:
+            logging.error("Exception Information",exc_info=True)
 
 @app.route('/employee/<int:emp_id>',methods=['GET'])
 def get_spec_emp(emp_id):
-    conn = sqlite3.connect('empdatabase.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
-    data = cursor.fetchall()
-    conn.close()
-    
-    
-    result = [{'id': row[0], 'name': row[1], 'address':row[2], 'city':row[3], 'salary':row[4] } for row in data]
+    app.logger.info(f"User requested a GET request for employee-id:{emp_id} ")
+    try:
+        conn = sqlite3.connect('empdatabase.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM emp WHERE id = ?', (emp_id,))
+        data = cursor.fetchall()
+        conn.close()
 
-    if not result:
-        return jsonify({'error': 'Employee not found'}), 404
+        result = [{'id': row[0], 'name': row[1], 'address':row[2], 'city':row[3], 'salary':row[4] } for row in data]
+
+        if not result:
+            app.logger.info("Employee not found in the database")
+            return jsonify({'error': 'Employee not found'}), 404
+
+        app.logger.info("Data fetched sucessfully from the database")
+        return jsonify(result)
     
-    return jsonify(result)
+    except Exception as obj:
+            logging.error("Exception Information",exc_info=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
